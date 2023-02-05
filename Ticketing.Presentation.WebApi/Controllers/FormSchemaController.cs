d3a6e5356106b05ee9f0294f6ebb02437bcd9ded
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-05 14:52:28 +0330

    Create FormSchema Controller in WebApi

---
+++ Ticketing.Presentation.WebApi/Controllers/FormSchemaController.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.Application.Contracts.FormSchema;
+using Ticketing.Application.Contracts.FormSchema.Commands;
+using Ticketing.Application.Contracts.FormSchema.ViewModels;
+
+namespace Ticketing.Presentation.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormSchemaController : ControllerBase
+    {
+        private readonly IFormSchemaApplication application;
+        public FormSchemaController(IFormSchemaApplication application)
+        {
+            this.application = application;
+        }
+
+        [HttpGet]
+        public IList<FormSchemaViewModel> GetAll() => application.GetAll();
+
+        [HttpGet("{id}")]
+        public FormSchemaViewModel Get(int id) => application.GetBy(id);
+
+        [HttpPost]
+        public void Add([FromBody] NewFormSchemaCommand command) => application.Add(command);
+
+        [HttpPost]
+        public void Update([FromBody] UpdateFormSchemaCommand command) => application.Update(command);
+
+        [HttpPost]
+        public void Active(int id) => application.Active(id);
+
+        [HttpPost]
+        public void DeActive(int id) => application.DeActive(id);
+    }
+}
