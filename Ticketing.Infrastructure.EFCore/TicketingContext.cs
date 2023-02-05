c55e9270368cad3ac63cbb4edf6fc72af689363d
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-05 13:01:56 +0330

    Create Database Context

---
+++ Ticketing.Infrastructure.EFCore/TicketingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Domain.FormSchemaAgg;
+using Ticketing.Domain.FormSchemaFieldAgg;
+
+namespace Ticketing.Infrastructure.EFCore;
+
+public class TicketingContext : DbContext
+{
+    public TicketingContext(DbContextOptions<TicketingContext> options) : base(options) { }
+
+    public DbSet<FormSchema> FormSchemas { get; set; }
+    public DbSet<FormSchemaField> FormSchemasFields { get; set; }
+}
