cccfa965a5ae78cf252955ded0c707b14689f815
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-05 13:04:22 +0330

    Create FormSchema Mapping

---
+++ Ticketing.Infrastructure.EFCore/Mappings/FormSchemaMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ticketing.Domain.FormSchemaAgg;
+
+namespace Ticketing.Infrastructure.EFCore.Mappings;
+
+public class FormSchemaMapping : IEntityTypeConfiguration<FormSchema>
+{
+    public void Configure(EntityTypeBuilder<FormSchema> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Title).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(300);
+        builder.Property(x => x.IsActive).IsRequired();
+
+        builder.HasMany(x => x.Fields).WithOne(x => x.FormSchema).HasForeignKey(x => x.FormSchemaId);
+    }
+}
