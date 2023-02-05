ffb9a75f5788ed5be2e94c452a46502ba99d9fba
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-05 15:10:08 +0330

    Add FormSchemaField Mapping

---
+++ Ticketing.Infrastructure.EFCore/Mappings/FormSchemaFieldMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ticketing.Domain.FormSchemaFieldAgg;
+
+namespace Ticketing.Infrastructure.EFCore.Mappings;
+
+public class FormSchemaFieldMapping : IEntityTypeConfiguration<FormSchemaField>
+{
+    public void Configure(EntityTypeBuilder<FormSchemaField> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Title).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.Description).HasMaxLength(300);
+        builder.Property(x => x.IsActive).IsRequired();
+        builder.Property(x => x.FormSchemaId).IsRequired();
+
+        builder.HasOne(x => x.FormSchema).WithMany(x => x.Fields).HasForeignKey(x => x.FormSchemaId);
+    }
+}
