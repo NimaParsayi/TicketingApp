f2c05ac68fab926db28cd373eb004aa7fdd11c65
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-06 18:45:20 +0330

    Implement IFormSchemaFieldApplication

---
+++ Ticketing.Application/FormSchemaFieldApplication.cs
@@ -0,0 +1,69 @@
+﻿using Ticketing.Application.Contracts.FormSchema.ViewModels;
+using Ticketing.Application.Contracts.FormSchemaField;
+using Ticketing.Application.Contracts.FormSchemaField.Commands;
+using Ticketing.Application.Contracts.FormSchemaField.ViewModels;
+using Ticketing.Domain.FormSchemaAgg;
+using Ticketing.Domain.FormSchemaAgg.Services;
+using Ticketing.Domain.FormSchemaFieldAgg;
+
+namespace Ticketing.Application;
+
+public class FormSchemaFieldApplication: IFormSchemaFieldApplication
+{
+    private readonly IFormSchemaFieldRepository formSchemaFieldRepository;
+    private readonly IFormSchemaFieldValidator formSchemaFieldValidator;
+    public IList<FormSchemaFieldViewModel> GetAll()
+    {
+        var list = new List<FormSchemaFieldViewModel>();
+        foreach (var formSchemaField in formSchemaFieldRepository.Get())
+        {
+            list.Add(new FormSchemaFieldViewModel()
+            {
+                Id = formSchemaField.Id,
+                Title = formSchemaField.Title,
+                Description = formSchemaField.Description,
+                CreationDate = formSchemaField.CreationDate.ToShortDateString(),
+                FormSchemaId = formSchemaField.FormSchemaId
+            });
+        }
+
+        return list;
+    }
+
+    public FormSchemaFieldViewModel GetBy(int id)
+    {
+        var formSchemaField = formSchemaFieldRepository.GetBy(id);
+        return new FormSchemaFieldViewModel()
+        {
+            Id = formSchemaField.Id,
+            Title = formSchemaField.Title,
+            Description = formSchemaField.Description,
+            CreationDate = formSchemaField.CreationDate.ToShortDateString(),
+            FormSchemaId = formSchemaField.FormSchemaId
+        };
+    }
+
+    public void Add(NewFormSchemaFieldCommand command)
+    {
+        formSchemaFieldRepository.Add(new FormSchemaField(command.Title, command.Description, command.FormSchemaId, formSchemaFieldValidator));
+        formSchemaFieldRepository.Save();
+    }
+
+    public void Update(UpdateFormSchemaFieldCommand command)
+    {
+        formSchemaFieldRepository.GetBy(command.Id).Update(command.Title, command.Description, formSchemaFieldValidator);
+        formSchemaFieldRepository.Save();
+    }
+
+    public void Active(int formSchemaFieldId)
+    {
+        formSchemaFieldRepository.GetBy(formSchemaFieldId).Active();
+        formSchemaFieldRepository.Save();
+    }
+
+    public void DeActive(int formSchemaFieldId)
+    {
+        formSchemaFieldRepository.GetBy(formSchemaFieldId).DeActive();
+        formSchemaFieldRepository.Save();
+    }
+}
--- Ticketing.Domain/FormSchemaFieldAgg/IFormSchemaFieldRepository.cs
+++ Ticketing.Domain/FormSchemaFieldAgg/IFormSchemaFieldRepository.cs
@@ -6,8 +6,8 @@ public interface IFormSchemaFieldRepository
 {
     IList<FormSchemaField> Get();
     FormSchemaField GetBy(int id);
-    void Add(FormSchema formSchema);
-    void Update(FormSchema formSchema);
+    void Add(FormSchemaField formSchemaField);
+    void Update(FormSchemaField formSchemaField);
     void Active(int id);
     void DeActive(int id);
     void Save();
