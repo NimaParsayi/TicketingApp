dab3148adc00e6f4c1f8902ea82f3e231f4112d0
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-06 18:48:49 +0330

    Implement IFormSchemaFieldRepository

---
+++ Ticketing.Infrastructure.EFCore/Services/FormSchemaFieldRepository.cs
@@ -0,0 +1,35 @@
+﻿using Ticketing.Domain.FormSchemaFieldAgg;
+
+namespace Ticketing.Infrastructure.EFCore.Services;
+
+public class FormSchemaFieldRepository : IFormSchemaFieldRepository
+{
+    private readonly TicketingContext context;
+    public FormSchemaFieldRepository(TicketingContext context)
+    {
+        this.context = context;
+    }
+
+    public IList<FormSchemaField> Get() => context.FormSchemasFields.ToList();
+
+    public FormSchemaField GetBy(int id) => context.FormSchemasFields.Find(id);
+
+    public void Add(FormSchemaField formSchemaField) => context.FormSchemasFields.Add(formSchemaField);
+
+    public void Update(FormSchemaField formSchemaField)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Active(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeActive(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save() => context.SaveChangesAsync();
+}
