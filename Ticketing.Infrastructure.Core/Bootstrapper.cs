67a6d4937b5fbbb8436a9b8b370a54505b97b9d0
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-06 18:31:15 +0330

    Change Bootstrapper File Structure

--- Ticketing.Infrastructure.Core/Bootstrapper.cs
+++ Ticketing.Infrastructure.Core/Bootstrapper.cs
@@ -14,12 +14,25 @@ namespace Ticketing.Infrastructure.Core
         public static void Set(IServiceCollection services, string connectionString)
         {
             // Database Services
+
+            #region  Database Services
+
             services.AddDbContext<TicketingContext>(options => options.UseSqlServer(connectionString));
             services.AddTransient<IFormSchemaRepository, FormSchemaRepository>();
 
-            // Applications
+            #endregion
+
+            #region Applications
+
+            // FormSchema
             services.AddTransient<IFormSchemaValidator, FormSchemaValidator>();
             services.AddTransient<IFormSchemaApplication, FormSchemaApplication>();
+
+            // FormSchemaField
+            services.AddTransient<IFormSchemaFieldValidator, FormSchemaFieldValidator>();
+            //services.AddTransient<IFormSchemaFieldApplication, FormSchemaFieldApplication>();
+
+            #endregion
         }
     }
 }
