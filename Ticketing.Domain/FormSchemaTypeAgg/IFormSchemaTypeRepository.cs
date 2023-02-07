30f055a2e386d1da2769173f5fecb1ff1e2c0ad1
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-07 15:30:27 +0330

    Create IFormSchemaTypeRepository

---
+++ Ticketing.Domain/FormSchemaTypeAgg/IFormSchemaTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticketing.Domain.FormTypeAgg;
+
+public interface IFormSchemaTypeRepository
+{
+    List<FormSchemaType> Get();
+    FormSchemaType GetBy(int id);
+    void Add(FormSchemaType formSchemaType);
+    void Save();
+}
