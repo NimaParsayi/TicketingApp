80bdf6013c2edc9bf38cd514b59a06993739cd3b
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-09 18:03:22 +0330

    Add Form Model

---
+++ Ticketing.Domain/FormAgg/Form.cs
@@ -0,0 +1,33 @@
+﻿namespace Ticketing.Domain.FormAgg;
+
+public class Form
+{
+    #region Properties
+
+    public int Id { get; private set; }
+    public string Mobile { get; private set; }
+    public object Data { get; private set; }
+    public DateTime CreationDate { get; private set; }
+
+
+    #endregion
+
+    #region Constructors
+
+    protected Form() { }
+
+    public Form(string mobile, object data)
+    {
+        Mobile = mobile;
+        Data = data;
+        CreationDate = DateTime.Now;
+    }
+
+    #endregion
+
+    #region Methods
+
+    
+
+    #endregion
+}
