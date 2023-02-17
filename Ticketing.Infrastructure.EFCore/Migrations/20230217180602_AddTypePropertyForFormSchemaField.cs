8ea3a5f3337e7e5ccea77cf97d3786a1846fcc05
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-17 21:43:32 +0330

    Add FormSchemaField Type Migration

---
+++ Ticketing.Infrastructure.EFCore/Migrations/20230217180602_AddTypePropertyForFormSchemaField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketing.Infrastructure.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTypePropertyForFormSchemaField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "Type",
+                table: "FormSchemasFields",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "FormSchemasFields");
+        }
+    }
+}
