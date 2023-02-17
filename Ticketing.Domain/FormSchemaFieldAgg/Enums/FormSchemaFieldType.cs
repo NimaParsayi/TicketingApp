e9edecddfe617e6fb60d2f752ee9429596629480
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-17 21:56:08 +0330

    Remove short Value for FormSchemaFieldType Enum

--- Ticketing.Domain/FormSchemaFieldAgg/Enums/FormSchemaFieldType.cs
+++ Ticketing.Domain/FormSchemaFieldAgg/Enums/FormSchemaFieldType.cs
@@ -1,10 +1,10 @@
 ﻿namespace Ticketing.Domain.FormSchemaFieldAgg.Enums;
 
-public enum FormSchemaFieldType : short
+public enum FormSchemaFieldType
 {
-    Text = 0,
-    Number = 1,
-    Mobile = 2,
-    Chat = 3,
-    Calendar = 4,
+    Text,
+    Number,
+    Mobile,
+    Chat,
+    Calendar,
 }
