7ad1d0bb1a24887c6bfccb7a042fb02033d7be1d
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-19 21:22:44 +0330

    Create IFormReplyRepository Structure

---
+++ Ticketing.Domain/FormReplyAgg/IFormReplyRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Ticketing.Domain.FormReplyAgg;
+
+public interface IFormReplyRepository
+{
+    IList<FormReply> Get();
+    FormReply GetBy(long id);
+    IList<FormReply> GetBy(string mobile);
+    void Add(FormReply formReply);
+    void Save();
+}
