72b19a036f968123f4c5e35e4ca16f03308ebd26
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-19 16:37:23 +0330

    Migrate Form to Database

--- Ticketing.Infrastructure.EFCore/Migrations/20230219130655_AddFormTable.Designer.cs
+++ Ticketing.Infrastructure.EFCore/Migrations/20230219130655_AddFormTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ticketing.Infrastructure.EFCore;
 
@@ -11,9 +12,11 @@ using Ticketing.Infrastructure.EFCore;
 namespace Ticketing.Infrastructure.EFCore.Migrations
 {
     [DbContext(typeof(TicketingContext))]
-    partial class TicketingContextModelSnapshot : ModelSnapshot
+    [Migration("20230219130655_AddFormTable")]
+    partial class AddFormTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
