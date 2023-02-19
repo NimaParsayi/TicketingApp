dbb5fc991fbf4d4140239323b5edb58ae2b0b9d1
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-19 18:03:44 +0330

    Fix GetBy Method in FormSchemaTypeRepository

--- Ticketing.Infrastructure.EFCore/Services/FormSchemaTypeRepository.cs
+++ Ticketing.Infrastructure.EFCore/Services/FormSchemaTypeRepository.cs
@@ -1,4 +1,5 @@
-﻿using Ticketing.Domain.FormSchemaTypeAgg;
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Domain.FormSchemaTypeAgg;
 
 namespace Ticketing.Infrastructure.EFCore.Services;
 
@@ -12,7 +13,7 @@ public class FormSchemaTypeRepository : IFormSchemaTypeRepository
 
     public List<FormSchemaType> Get() => context.FormSchemaTypes.ToList();
 
-    public FormSchemaType GetBy(int id) => context.FormSchemaTypes.Find(id);
+    public FormSchemaType GetBy(int id) => context.FormSchemaTypes.Include(x=>x.Schema).Include(x=>x.Schema.Fields).FirstOrDefault(x=>x.Id == id);
 
     public void Add(FormSchemaType formSchemaType) => context.Add(formSchemaType);
 
