c75d8d17ccf28a268ffd79c566eb947ca823eab3
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-21 00:42:12 +0330

    Add "FormReply" Table in EFCore Database

---
+++ Ticketing.Infrastructure.EFCore/Migrations/20230220211024_AddFormReplyTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketing.Infrastructure.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFormReplyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormsReplies",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mobile = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    FormId = table.Column<int>(type: "int", nullable: false),
+                    ReplyToMessageId = table.Column<long>(type: "bigint", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormsReplies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormsReplies_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormsReplies_FormId",
+                table: "FormsReplies",
+                column: "FormId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormsReplies");
+        }
+    }
+}
