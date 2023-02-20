c29dcf7873d552983fecce3aa99de79833f7954a
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-20 10:28:33 +0330

    Add "Add Page" in FormSchemaType in RazorPages Application

---
+++ Ticketing.Presentation.RazorPages/Pages/FormSchemaType/Add.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ticketing.Application.Contracts.FormSchemaType;
+using Ticketing.Application.Contracts.FormSchemaType.Commands;
+
+namespace Ticketing.Presentation.RazorPages.Pages.FormSchemaType
+{
+    public class AddModel : PageModel
+    {
+        private readonly IFormSchemaTypeApplication application;
+        public AddModel(IFormSchemaTypeApplication application)
+        {
+            this.application = application;
+        }
+
+        [BindProperty]
+        public NewFormSchemaTypeCommand Command { get; set; }
+
+        public void OnGet()
+        {
+            Command = new NewFormSchemaTypeCommand();
+        }
+
+        public void OnPost()
+        {
+            application.Add(Command);
+            RedirectToPage("/FormSchemaType/Index");
+        }
+    }
+}
