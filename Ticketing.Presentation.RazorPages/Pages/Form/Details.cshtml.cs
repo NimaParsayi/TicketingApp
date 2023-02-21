43bbf1c493f766dcfeb7961a799909bb806dc3d3
Nima Parsayi <nimaprs.ps@outlook.com>
2023-02-21 10:53:14 +0330

    Add "Details Page" for Forms in RazorPages

--- Ticketing.Presentation.RazorPages/Pages/Form/Details.cshtml.cs
+++ Ticketing.Presentation.RazorPages/Pages/Form/Details.cshtml.cs
@@ -1,23 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 using Ticketing.Application.Contracts.Form;
 using Ticketing.Application.Contracts.Form.ViewModels;
+using Ticketing.Application.Contracts.FormReply;
+using Ticketing.Application.Contracts.FormReply.Commands;
 
 namespace Ticketing.Presentation.RazorPages.Pages.Form
 {
     public class DetailsModel : PageModel
     {
         private readonly IFormApplication application;
-        public DetailsModel(IFormApplication application)
+        private readonly IFormReplyApplication replyApplication;
+        public DetailsModel(IFormApplication application, IFormReplyApplication replyApplication)
         {
             this.application = application;
+            this.replyApplication = replyApplication;
         }
 
         public FormViewModel Form { get; set; }
+        public IDictionary<string, object> Fields { get; set; }
+        [BindProperty]
+        public NewReplyCommand Command { get; set; }
 
         public void OnGet(int id)
         {
             Form = application.GetBy(id);
+            Fields = JsonConvert.DeserializeObject<IDictionary<string, object>>(Form.Data.ToString());
+            Command = new NewReplyCommand();
+        }
+
+        public IActionResult OnPost()
+        {
+            replyApplication.Add(Command);
+            return RedirectToPage("/Form/Index");
         }
     }
 }
